af3c7b1ecf69ca56f89d1ab211bd0b59a64e3cb4
Ruan Swanepoel <ruane.swanepoel@gmail.com>
2025-10-01 19:18:28 +1000

    Bug fix

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function parseInfoString(info) {
 }
 
 function preprocess(text, opts) {
-  const defaultWidth = opts.fenchdTxtWrapWidth || 80;
+  const defaultWidth = opts.fencedTxtWrapWidth || 80;
 
   return text.replace(/```txt(.*?)\n([\s\S]*?)```/g, (match, info, code) => {
     const blockOptions = parseInfoString(info);
